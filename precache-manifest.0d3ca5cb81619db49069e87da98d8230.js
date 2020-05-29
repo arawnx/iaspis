96a7028770803d23e764729beed502d937c05f75
Xander Xylona <sashasylona@gmail.com>
2020-05-29 11:32:07 -0400

    Updates

---
+++ precache-manifest.0d3ca5cb81619db49069e87da98d8230.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e18fddf16a2a92a8b9e4a2493e4c3951",
+    "url": "/iaspis/index.html"
+  },
+  {
+    "revision": "26ab82b6f12b05480033",
+    "url": "/iaspis/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "050c019b5da812b2fa53",
+    "url": "/iaspis/static/js/2.05057cec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iaspis/static/js/2.05057cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26ab82b6f12b05480033",
+    "url": "/iaspis/static/js/main.388985dd.chunk.js"
+  },
+  {
+    "revision": "fd824e950bee575b6156",
+    "url": "/iaspis/static/js/runtime-main.1d0b758a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/iaspis/static/media/logo.5d5d9eef.svg"
+  }
+]);
