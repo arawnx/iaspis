321ee6879d4e636019e8f5fd86fbed56465bdc5a
Xander Xylona <sashasylona@gmail.com>
2020-06-07 20:45:46 -0400

    Fix security issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import KeyboardEventHandler from 'react-keyboard-event-handler';
-import lexicon from './20.json';
+import lexicon from './wordlist.json';
 import './App.css';
 
 class TextInput extends React.Component {
@@ -19,6 +19,7 @@ class TextInput extends React.Component {
 			if(e.target.value.charAt(e.target.value.length-1) !== ' ') {
 				this.inputRef.current.value = e.target.value.substring(0, e.target.value.length-1);
 				this.inputRef.current.style.backgroundColor = "rgba(200,0,0,0.4)";
+				this.props.scoreCallback(-0.15);
 				setTimeout(() => {
 					this.inputRef.current.style.backgroundColor = "transparent";
 				}, 1000);
@@ -30,69 +31,77 @@ class TextInput extends React.Component {
 		}
 
 		if(e.target.value === this.props.target) {
-			this.props.callback();
+			this.props.fillCallback();
+			this.props.scoreCallback(0.2);
 			this.inputRef.current.value = '';
 		}
 	}
 
+	computeStyle = () => {
+		return {
+			fontSize: this.props.textWidth.toString() + "vmin"
+		};
+	}
+
 	render() {
 		return (
-			<label className="text">
+			<label style={this.computeStyle()} className="text">
 				{this.props.target}
-				<input spellcheck="false" id="input" type="text" ref={this.inputRef} onChange={this.tookInput} className="text"/>
+				<input spellcheck="false" style={this.computeStyle()} id="input" type="text" ref={this.inputRef} onChange={this.tookInput} className="text"/>
 			</label>
 		);
 	}
 }
 
 TextInput.propTypes = {
-	target: PropTypes.string,
-	callback: PropTypes.func
+	target: PropTypes.string.isRequired,
+	textWidth: PropTypes.number.isRequired,
+	fillCallback: PropTypes.func.isRequired,
+	scoreCallback: PropTypes.func.isRequired
 };
-
-function shuffle(passes, arr) {
-	let res = arr.slice(0);
-	for(;passes>0;passes--) {
-		for(let i = 0; i < res.length; i++) {
-			if(Math.random() <= 0.5) {
-				let ch = res[i];
-				res.splice(i, 1);
-				res.unshift(ch);
-			}
-		}
-	}
-	return res;
-}
-
-function zip(a1, a2) {
-	return a1.map((c, i) => {
-		return "" + c + a2[i];
-	});
-}
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			lexicon: this.genLexicon(),
+			target: this.randSel(1),
+			score: 1,
+			textWidth: 70,
 			begun: false
 		};
-		console.log(lexicon);
 	}
 
-	genLexicon = () => {
-		let letters	= shuffle(5, "qwertyuiopasdfghjklzxcvbnm".split(""));
-		let dual	= zip(letters, shuffle(5, letters));
-		let triple	= zip(dual, shuffle(5, letters));
-		let quadruple	= zip(triple, shuffle(5, letters));
-		let quintuple	= zip(quadruple, shuffle(5, letters));
-		return letters.concat(dual).concat(triple).concat(quadruple).concat(quintuple);
+	randSel = (level) => {
+		level = Math.floor(level);
+		let lvlwords = lexicon[level.toString()];
+		return lvlwords[Math.floor(Math.random() * lvlwords.length)];
 	}
 
 	reloadTarget = () => {
 		this.setState({
-			lexicon: this.state.lexicon.slice(1)
+			target: this.randSel(this.state.score),
+			textWidth: this.computeTextWidth()
 		});
+	}
+
+	computeTextWidth = () => {
+		return Math.min(Math.floor(100/Math.floor(this.state.score)), 60);
+	}
+
+	mutateScore = (diff) => {
+		if(this.state.score + diff < 1) {
+			this.setState({
+				score: 1
+			});
+		} else if(this.state.score + diff > 19) {
+			this.setState({
+				score: 19
+			});
+		} else {
+			this.setState({
+				score: this.state.score + diff
+			});
+		}
 	}
 
 	begin = () => {
@@ -104,7 +113,7 @@ class App extends React.Component {
 	render() {
 		if(this.state.begun) {
 			return (
-				<TextInput callback={this.reloadTarget} target={this.state.lexicon[0]}/>
+				<TextInput fillCallback={this.reloadTarget} scoreCallback={this.mutateScore} textWidth={this.state.textWidth} target={this.state.target}/>
 			);
 		} else {
 			return (
